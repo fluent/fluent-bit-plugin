761dd4d02f15d8607dfe4dc8c050629161404785
Eduardo Silva <eduardo@treasure-data.com>
2019-06-20 16:37:28 -0600

    Initial import
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

---
+++ README.md
@@ -0,0 +1,49 @@
+# Fluent Bit Plugin
+
+The following repository provides the structure and build-system helpers to develop C dynamic plugins for [Fluent Bit](https://fluentbit.io) like _inputs_, _filters_ and _outputs_.
+
+>  Fluent Bit API development/usage is out of the scope of this article.
+
+## Requirements
+
+- [Fluent Bit](https://fluentbit.io) Source code, version >= 1.2
+- C compiler: GCC or Clang
+- CMake3
+
+## Getting Started
+
+In the following steps we will build the example plugin provided called __out_stdout2__. As a first step get into the _build/_ directory:
+
+```bash
+$ cd build/
+```
+
+Now we will provide CMake (our build system) two important variables:
+
+- FLB\_SOURCE: absolute path to source code of Fluent Bit.
+- PLUGIN\_NAME: _directory_ name of the project that we aim to build. Note that any plugin name must have it proper prefix as the example mentioned above.
+
+Assuming that Fluent Bit source code is located at /tmp/fluent-bit and we will build _out\_stdout2_, we will run CMake with the following options:
+
+```bash
+$ cmake -DFLB_SOURCE=/tmp/fluent-bit -DPLUGIN_NAME=out_stdout2 ../
+```
+
+then type 'make' to build the plugin:
+
+```
+$ make
+Scanning dependencies of target flb-out_stdout2
+[ 50%] Building C object out_stdout2/CMakeFiles/flb-out_stdout2.dir/stdout2.c.o
+[100%] Linking C shared library ../flb-out_stdout2.so
+[100%] Built target flb-out_stdout2
+```
+
+If you query the content of the current directory you will find the new shared library created:
+
+```
+$ ls -l *.so
+-rwxr-xr-x 1 edsiper edsiper 17288 jun 20 16:33 flb-out_stdout2.so
+```
+
+that __.so__ file is our dynamic plugin that now can be loaded from Fluent Bit through the [plugins configuration](https://github.com/fluent/fluent-bit/blob/master/conf/plugins.conf) file.
---
+++ out_stdout2/stdout2.c
@@ -0,0 +1,55 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2019      The Fluent Bit Authors
+ *  Copyright (C) 2015-2018 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include <fluent-bit/flb_info.h>
+#include <fluent-bit/flb_output.h>
+#include <fluent-bit/flb_pack.h>
+
+static int cb_stdout_init(struct flb_output_instance *ins,
+                          struct flb_config *config, void *data)
+{
+    (void) ins;
+    (void) config;
+    (void) data;
+}
+
+static void cb_stdout_flush(const void *data, size_t bytes,
+                            const char *tag, int tag_len,
+                            struct flb_input_instance *i_ins,
+                            void *out_context,
+                            struct flb_config *config)
+{
+    flb_pack_print(data, bytes);
+    FLB_OUTPUT_RETURN(FLB_OK);
+}
+
+static int cb_stdout_exit(void *data, struct flb_config *config)
+{
+    return 0;
+}
+
+struct flb_output_plugin out_stdout2_plugin = {
+    .name         = "stdout2",
+    .description  = "Prints events to STDOUT",
+    .cb_init      = cb_stdout_init,
+    .cb_flush     = cb_stdout_flush,
+    .cb_exit      = cb_stdout_exit,
+    .flags        = 0,
+};
